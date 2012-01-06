fa8e8a695ea4a25c5e8716916bc74bf4832a287b
rajiv.narula@gmail.com <rajiv.narula@gmail.com@d991403e-5166-ad2f-a896-9f553e947079>
2012-01-06 12:26:00 +0000

    fixed a typo

--- trunk/run.groovy
+++ trunk/run.groovy
@@ -1,30 +1,30 @@
-def antBbuilder=  new AntBuilder()
-antBbuilder.property ( 'file' : 'cobertura.properties' )
-antBbuilder.property ( 'name' : 'project.base' , 'value' : '.' )
-antBbuilder.property ( 'name' : 'cobertura.dir' , 'value' : './lib/cobertura-1.9.4.1' )
-antBbuilder.property ( 'name' : 'datafile' , 'value' : 'cobertura.ser' )
-antBbuilder.path ( 'id' : 'cobertura.classpath' ) {
+def antBuilder=  new AntBuilder()
+antBuilder.property ( 'file' : 'cobertura.properties' )
+antBuilder.property ( 'name' : 'project.base' , 'value' : '.' )
+antBuilder.property ( 'name' : 'cobertura.dir' , 'value' : './lib/cobertura-1.9.4.1' )
+antBuilder.property ( 'name' : 'datafile' , 'value' : 'cobertura.ser' )
+antBuilder.path ( 'id' : 'cobertura.classpath' ) {
     fileset ( 'dir' : '${cobertura.dir}' ) {
       include ( 'name' : 'cobertura.jar' )
       include ( 'name' : 'lib/**/*.jar' )
     }
   }
-antBbuilder.path ( 'id' : 'test.classpath' ) {
+antBuilder.path ( 'id' : 'test.classpath' ) {
     fileset ( 'dir' : '${war.dir}' ) {
       include ( 'name' : '**/${war.file}' )
     }
   }
-antBbuilder.taskdef ( 'classpathref' : 'cobertura.classpath' , 'resource' : 'tasks.properties' )
+antBuilder.taskdef ( 'classpathref' : 'cobertura.classpath' , 'resource' : 'tasks.properties' )
 
 
-def report(antBbuilder){
-	antBbuilder.'cobertura-report' ( 'destdir' : '${coveragereport.dir}' , 'srcdir' : '${src.dir}' , 'datafile' : '${datafile}' , 'format' : 'html' )
+def report(antBuilder){
+	antBuilder.'cobertura-report' ( 'destdir' : '${coveragereport.dir}' , 'srcdir' : '${src.dir}' , 'datafile' : '${datafile}' , 'format' : 'html' )
 }
 
 
-def instrumentWar(antBbuilder){
+def instrumentWar(antBuilder){
 
-  antBbuilder.'cobertura-instrument' ( 'todir' : '${instrumented.dir}' , 'datafile' : '${datafile}' ) {
+  antBuilder.'cobertura-instrument' ( 'todir' : '${instrumented.dir}' , 'datafile' : '${datafile}' ) {
     includeClasses ( 'regex' : '${include.regex}' )
     instrumentationClasspath {
       path ( 'refid' : 'test.classpath' )
@@ -34,39 +34,39 @@ def instrumentWar(antBbuilder){
 }
 
 
-def injectCoberturaWar(antBbuilder){
-	antBbuilder.unzip(src:'${instrumented.dir}'+'/${war.file}', dest:'stagingWAR') 
-	antBbuilder.copy(file:'lib/cobertura-1.9.4.1/cobertura.jar', tofile:'stagingWAR/WEB-INF/lib/cobertura.jar')
-	antBbuilder.zip(destfile:'${instrumented.dir}/${war.file}', basedir:'stagingWAR')
+def injectCoberturaWar(antBuilder){
+	antBuilder.unzip(src:'${instrumented.dir}'+'/${war.file}', dest:'stagingWAR') 
+	antBuilder.copy(file:'lib/cobertura-1.9.4.1/cobertura.jar', tofile:'stagingWAR/WEB-INF/lib/cobertura.jar')
+	antBuilder.zip(destfile:'${instrumented.dir}/${war.file}', basedir:'stagingWAR')
 }
 
 
-def cleanDataFile (antBbuilder){
-  antBbuilder.delete ( 'file' : '${datafile}' )
+def cleanDataFile (antBuilder){
+  antBuilder.delete ( 'file' : '${datafile}' )
 }
 
-def cleanInstrumented (antBbuilder){
-  antBbuilder.delete ( 'dir' : '${instrumented.dir}' )
-  antBbuilder.mkdir ( 'dir' : '${instrumented.dir}' )
+def cleanInstrumented (antBuilder){
+  antBuilder.delete ( 'dir' : '${instrumented.dir}' )
+  antBuilder.mkdir ( 'dir' : '${instrumented.dir}' )
 }
 
-def cleanReport (antBbuilder){
-  antBbuilder.delete ( 'dir' : '${coveragereport.dir}' )
+def cleanReport (antBuilder){
+  antBuilder.delete ( 'dir' : '${coveragereport.dir}' )
 }
 
-def instrument (antBbuilder){
-	cleanInstrumented (antBbuilder)
-	instrumentWar(antBbuilder)
-	injectCoberturaWar(antBbuilder)
+def instrument (antBuilder){
+	cleanInstrumented (antBuilder)
+	instrumentWar(antBuilder)
+	injectCoberturaWar(antBuilder)
 }
 
 def command= args[0]
 
 
 switch (command){
-	case 'instrument': instrument (antBbuilder)
+	case 'instrument': instrument (antBuilder)
 			break
-	case 'report' : report (antBbuilder)
+	case 'report' : report (antBuilder)
 		break
 	default: println 'Unknown command. usage: groovy run <instrument or report>'
 
